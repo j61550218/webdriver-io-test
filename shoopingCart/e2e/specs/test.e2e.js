0518015fcacce84080c4c55f271285f5f3a211d1
jose <josedanielqqn996@gmail.com>
2025-03-03 11:29:06 -0400

    new change

--- shoopingCart/e2e/specs/test.e2e.js
+++ shoopingCart/e2e/specs/test.e2e.js
@@ -6,13 +6,13 @@ describe('Shopping Cart', () => {
 
     it('add product to cart and remove it', async () => {
         await browser.url('http://opencart.abstracta.us')
-        //await browser.saveScreenshot('./screenshots/home.png');
+        await browser.saveScreenshot('./screenshots/home.png');
 
         await HomePage.searchInput.setValue('iPhone')
-        //await browser.saveScreenshot('./screenshots/iphone-search.png');
+        await browser.saveScreenshot('./screenshots/iphone-search.png');
         await HomePage.searchButton.click()
         await browser.pause(3000)
-        //await browser.saveScreenshot('./screenshots/iphone-search-result.png');
+        await browser.saveScreenshot('./screenshots/iphone-search-result.png');
 
         await HomePage.searchResult.click()
         await browser.pause(3000)
@@ -21,17 +21,17 @@ describe('Shopping Cart', () => {
         await expect(HomePage.cartItemName).toHaveText('iPhone')
         await browser.pause(3000)
 
-        //await browser.saveScreenshot('./screenshots/cart-menu.png');
+        await browser.saveScreenshot('./screenshots/cart-menu.png');
         await HomePage.cartDropdownViewCartOption.click()
         await browser.pause(3000)
 
-        //await browser.saveScreenshot('./screenshots/checkout-cart.png');
+        await browser.saveScreenshot('./screenshots/checkout-cart.png');
         const itemText = await CheckoutPage.checkoutListItem.getText()
         await expect(itemText).toContain('iPhone')
         await CheckoutPage.removeItemButton.click()
         await browser.pause(3000)
         
-        //await browser.saveScreenshot('./screenshots/checkout-cart-remove.png');
+        await browser.saveScreenshot('./screenshots/checkout-cart-remove.png');
         const emptyCartText = await CheckoutPage.emptyShoppingCartTitle.getText()
         await expect(emptyCartText).toContain('Your shopping cart is empty!')
     })
